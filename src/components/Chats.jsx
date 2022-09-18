68c4f934250003269773173353bc08bc4b43f83b
Mohit <mohitsharma8864@gmail.com>
2022-09-18 16:09:54 +0530

    components

---
+++ src/components/Chats.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Chats() {
+  return (
+    <div className='chats'>
+        <div className="userChat">
+            <img src="https://images.unsplash.com/photo-1485875437342-9b39470b3d95?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1173&q=80" alt="" />
+            <div className="userChatInfo">
+                <span>Robert</span>
+                <p>Hello</p>
+            </div>
+        </div>
+    </div>
+  )
+}
